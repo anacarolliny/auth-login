37cd43c40ab55fffe8206d6e84d5069a49e52e07
anacarolliny <acss2011@hotmail.com>
2024-07-25 19:13:38 -0300

    add password logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/users.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: CreateUserDto) {
-    return this.prisma.user.create({
-      data,
-    });
-  }
+    const { email, password } = data;
 
+    const emailAlreadyExists = await this.emailExists(email);
+
+    if (emailAlreadyExists) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.prisma.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
+    return user;
+  }
   async getUsers() {
     return this.prisma.user.findMany();
+  }
+
+  async emailExists(email: string): Promise<boolean> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    return !!user;
   }
 }
